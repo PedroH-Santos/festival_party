41ea3d2d024442e5f6d0b8910712dd93cf6fea7e
Pedro Santos <pedro.henrique@construsitebrasil.com.br>
2022-08-11 10:58:19 -0300

    Cliente paginacao e pesquisa

--- src/shared/infra/http/routes/Client/client.routes.ts
+++ src/shared/infra/http/routes/Client/client.routes.ts
@@ -4,6 +4,7 @@ import { CreateClientController } from "@modules/client/useCases/createClient/Cr
 import { DeleteClientController } from "@modules/client/useCases/deleteClient/DeleteClientController";
 import { FindClientByIdController } from "@modules/client/useCases/findClientById/FindClientByIdController";
 import { ListClientController } from "@modules/client/useCases/listClient/ListClientController";
+import { ListClientWithPaginationController } from "@modules/client/useCases/listClientWithPagination/ListClientWithPaginationController";
 import {Router} from  "express";
 
 
@@ -13,8 +14,11 @@ const createClientController = new CreateClientController();
 const listClientController = new ListClientController();
 const deleteClientController = new DeleteClientController();
 const findClientByIdController = new FindClientByIdController();
+const listClientWithPaginationController = new ListClientWithPaginationController();
 clientRouter.post("/",createClientController.handle);
 clientRouter.get("/",listClientController.handle);
+clientRouter.get("/pagination",listClientWithPaginationController.handle);
+
 clientRouter.get("/detail/:id",findClientByIdController.handle);
 
 clientRouter.delete("/:id",deleteClientController.handle); 
